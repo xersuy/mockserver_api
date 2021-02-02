7a42d5f0dc7074961b7be6145d3ff81c9bf0ed72
xersuy <dev.luyoes20@gmail.com>
2021-02-02 23:45:12 +0900

    Update index.js

--- mocks/index.js
+++ mocks/index.js
@@ -6,4 +6,8 @@ const argv = require('yargs').argv;
 const portAPI = argv.portAPI || 9002;
 const directory = argv.dir || 'mocks';
 
+process.on('SIGINT', () => { console.log("Bye bye!"); process.exit(); });
+
+
+
 console.log("argv", argv)
