79b2c9bf7e4fb7adcf6d7242ce2a6a5eb0d9978a
xersuy <dev.luyoes20@gmail.com>
2021-02-04 20:35:35 +0900

    index server add

--- mocks/index.js
+++ mocks/index.js
@@ -3,11 +3,27 @@ const http = require('http');
 const mockserver = require('mockserver');
 
 const argv = require('yargs').argv;
-const portAPI = argv.portAPI || 9002;
+const portAPI = argv.portAPI || 10002;
 const directory = argv.dir || 'mocks';
+
 
 process.on('SIGINT', () => { console.log("Bye bye!"); process.exit(); });
 
+const apiServer = (directory, port, verbose = true) => {
 
+  http.createServer(mockserver(directory, verbose)).listen(port, (error) => {
+    if (error) {
+        console.log(`Mock server unhandled exception`, error);
+        return;
+    }
 
-console.log("argv", argv)
+    if (verbose) {
+        const url = `http://0.0.0.0:${port}`.green
+        console.log(`Mockserver serving ${directory} start: ${url}`);
+        // console.log('argv option Check ', argv);
+    }
+})
+}
+
+apiServer(directory, portAPI, true);
+
